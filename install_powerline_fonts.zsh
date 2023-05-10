b75807c0b38589a0598cdf1b5426ef9022811a6f
Jordan Gillard <jordan@blackcicada.info>
2023-05-09 21:08:08 -0400

    feat: add install_powerline_fonts.zsh script
    This commit adds a new Zsh script for installing Powerline fonts. The script checks if the fonts are already installed and, if not, downloads and installs them.

---
+++ install_powerline_fonts.zsh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+# Check if the powerline fonts directory exists
+if [[ ! -d "$HOME/.local/share/fonts/Powerline" ]]; then
+    echo "Installing Powerline fonts..."
+    # Create the directory for Powerline fonts
+    mkdir -p "$HOME/.local/share/fonts/Powerline"
+
+    # Download and install Powerline fonts
+    git clone https://github.com/powerline/fonts.git --depth=1 /tmp/fonts
+    /tmp/fonts/install.sh
+    rm -rf /tmp/fonts
+
+    echo "Powerline fonts installed."
+else
+    echo "Powerline fonts are already installed."
+fi
