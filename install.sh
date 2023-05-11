e7c8f8a2096c6bc76ade13685086525963f31d9d
Jordan Gillard <jordan-gillard@outlook.com>
2023-05-11 10:42:40 -0400

    bug: Use source to run installation scripts

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 echo "Installing Z shell & setting it as default..."
-./install_zsh_and_set_default.sh
+source ./install_zsh_and_set_default.sh
 
 echo "Installing Oh My Zsh..."
-./install_oh_my_zsh.zsh
+source ./install_oh_my_zsh.zsh
 
 echo "Installing Powerline fonts..."
-./install_powerline_fonts.zsh
+source ./install_powerline_fonts.zsh
 
 echo "Installing clang-format..."
-./install_clang_format.zsh
+source ./install_clang_format.zsh
 
 echo "Installation complete. Please restart your terminal for changes to take effect."
